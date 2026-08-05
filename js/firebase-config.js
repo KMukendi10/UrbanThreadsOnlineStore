6106dcaf1d71b0a59ed3cf44999c935d45dfddf7
KMukendi10 <125656620+KMukendi10@users.noreply.github.com>
2026-08-05 15:22:19 +0200

    Firestore database created

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,41 +1,23 @@
-// // firebase-config.js
-// // Initializes Firebase and exports the pieces other files need (auth, db)
+// firebase-config.js
 
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+// Import the Firebase services we need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDm_R-lKlF0nLx0evN7sRJ0cBug4nHFT8c",
-//   authDomain: "urbanthreadsonlinestore-f75f4.firebaseapp.com",
-//   projectId: "urbanthreadsonlinestore-f75f4",
-//   storageBucket: "urbanthreadsonlinestore-f75f4.firebasestorage.app",
-//   messagingSenderId: "732220252091",
-//   appId: "1:732220252091:web:fa1b569ab74bab4f726e9e",
-//   measurementId: "G-QNYHVCDX0N"
-// };
+// Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAk3tGMohIQKMj_mRp6ZlOwSEcnFtZKqZ4",
+  authDomain: "urbanthreadsstore-1a093.firebaseapp.com",
+  projectId: "urbanthreadsstore-1a093",
+  storageBucket: "urbanthreadsstore-1a093.firebasestorage.app",
+  messagingSenderId: "268375787140",
+  appId: "1:268375787140:web:dd685947b8060f21cca4d1"
+};
 
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
-
-//<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyC_ndngIquxt1iwmfAc7D0W9Wql8fCYYkA",
-    authDomain: "urbanthreadsonlinestore-4e1ff.firebaseapp.com",
-    projectId: "urbanthreadsonlinestore-4e1ff",
-    storageBucket: "urbanthreadsonlinestore-4e1ff.firebasestorage.app",
-    messagingSenderId: "676347200701",
-    appId: "1:676347200701:web:c182b3a7ee6d08d922e3d4"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-//</script>
+// Export Authentication and Firestore
+export const auth = getAuth(app);
+export const db = getFirestore(app);
